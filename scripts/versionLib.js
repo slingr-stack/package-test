df954ef8223ef49df4f636d52539194d50c4fdd2
Franco Ojeda <fojeda@slingr.io>
2023-06-06 14:08:44 -0300

    change version

--- scripts/versionLib.js
+++ scripts/versionLib.js
@@ -1,26 +1,7 @@
 exports.versionValue = function (a, b) {
-    return "Version Test v4.0.0";
+    return "Version Test v1.0.0";
 }
 
 exports.configTest = function () {
     return config.get("webhooksSharedKey");
-}
-
-exports.complexTest = function (a){
-    var obj  = {
-        value : 2,
-        msg : "{{message here}}: ",
-        other : '{}}}'
-    }
-    return obj.msg + a;
-}
-
-exports.depConfig = function(){
-    var response = dependencies.crypto.base64.configTest();
-    return response;
-}
-
-exports.setDependencyValue = function(value){
-    dependencies.crypto.config.set('webhooksSharedKey', value);
-    return "webhooksSharedKey configured on dependency with value:" + value
 }
