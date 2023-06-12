ee9f385875bf072c23b1c3bcbbc1b173a5a29dde
Franco Ojeda <fojeda@slingr.io>
2023-06-12 13:00:08 -0300

    change version

--- scripts/versionLib.js
+++ scripts/versionLib.js
@@ -1,5 +1,5 @@
-exports.versionValue = function (a, b) {
-    return "Version Test v3.0.0";
+exports.versionValue = function () {
+    return "Version Test v1.0.0";
 }
 
 exports.configTest = function () {
